773818705e911f18c24c7060e20e144204b2bb90
edewajosh <joshedewa@gmail.com>
2022-08-24 17:32:17 +0300

    Added navbar component

--- fin-app/src/App.js
+++ fin-app/src/App.js
@@ -1,9 +1,11 @@
+import NavBar from "./components/NavBar";
 
 
 const App = () => {
   return (
     <div className="App">
      <h1>Hello FinApp</h1>
+     <NavBar />
     </div>
   );
 }
---
+++ fin-app/src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+const NavBar = () => {
+  return (
+    <footer>
+        <div>NavBar</div>
+    </footer>
+  )
+}
+
+export default NavBar

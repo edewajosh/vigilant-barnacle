de6ad0a9f9bd864d276b3f6fdb34834856c32a29
edewajosh <joshedewa@gmail.com>
2022-08-25 22:05:35 +0300

    updated sidebar.jsx

--- fin-app/src/components/SideBar.jsx
+++ fin-app/src/components/SideBar.jsx
@@ -1,7 +1,36 @@
+import { useNavigate, useLocation} from 'react-router-dom';
+import { ReactComponent as OfferIcon } from '../assets/svg/localOfferIcon.svg';
+import { ReactComponent as ExploreIcon } from '../assets/svg/exploreIcon.svg';
+import { ReactComponent as PersonOutlineIcon } from '../assets/svg/personOutlineIcon.svg';
+
 const SideBar = () => {
+
+  const navigate = useNavigate()
+  const location = useLocation() 
+
+  const pathMatchRoute = (route) => {
+   if (route === location.pathname){
+       return true
+   }
+  }
   return (
     <div className="sidebar">
-        <h1>Menu</h1>
+        <h3>Menu</h3>
+        <div className="sidebarNav">
+          <ul className="sidebarListItems">
+            <li className="sidebarListItem" onClick={() => navigate('/')}>
+                <p className={pathMatchRoute('/') ? 'navbarListItemNameActive': 'navbarListItemName'}>Outgoing</p>
+            </li>
+            <li className="sidebarListItem" onClick={() => navigate('/about')}>
+                <p className={pathMatchRoute('/about') ? 'navbarListItemNameActive': 'navbarListItemName'}>Incoming</p>
+            </li>
+            <li className="sidebarListItem" onClick={() => navigate('/dashboard')}>
+                <p className={pathMatchRoute('/dashboard') ? 'navbarListItemNameActive': 'navbarListItemName'}>Service</p>
+            </li>
+            <li className="sidebarListItem" onClick={() => navigate('/dashboard')}>
+            </li>
+          </ul>
+        </div>
     </div>
   )
 }
